559c449b5166f5f144e425d91f9439804805c3fe
Ishan Vemireddy <91898895+RedTachyon19@users.noreply.github.com>
2023-06-01 19:04:57 -0500

    Update main.py
    code that supports pycord and will be part of 2.0.0 update

--- main.py
+++ main.py
@@ -1,120 +1,119 @@
 import discord
-from discord.ext import commands
-from discord import guild
-
-from discord_slash import SlashCommand, SlashContext
-from discord_slash.utils.manage_commands import create_choice, create_option
-import datetime
+from discord import default_permissions
 import os
-import keep_alive
+import datetime
 
-bot = commands.Bot(command_prefix="!j ")
-slash = SlashCommand(bot, sync_commands=True)
-bot.remove_command('help')
+bot = discord.Bot(
+  intents = discord.Intents.all()
+)
+
+@bot.event
+async def on_ready():
+  print("jalapeno bot is online")
 
 snipe_message_author = {}
 snipe_message_content = {}
-#snipe_message_file = {}
-
 
 @bot.event
 async def on_message_delete(message):
+  ## the date and time need fixing to correspond to the corect time zones
 
-    ## the date and time need fixing to correspond to the corect time zones
+  print(datetime.date.today())
+  print(message.author)
+  print(message.content)
+  print()
 
-    print(datetime.date.today())
-    print(message.author)
-    print(message.content)
-    print()
-
-    snipe_message_author[message.channel.id] = message.author
-    snipe_message_content[message.channel.id] = message.content
-
-    print(message.content)
-
-    f = open("logs.txt", "a")
-    f.write(message.content + "\n")
-    f.write(str(message.author) + "\n")
-    f.write(str(message.created_at) + "\n" + "\n")
-    f.close()
+  snipe_message_author[message.channel.id] = message.author
+  snipe_message_content[message.channel.id] = message.content
+  
+  f = open("logs_2.txt", "a")
+  f.write(message.content + "\n")
+  f.write(str(message.author) + "\n")
+  f.write(str(message.created_at) + "\n" + "\n")
+  f.close()
 
 
-@slash.slash(name='ping', description='replies with a pong', guild_ids=[])
-async def ping(ctx: SlashContext):
-    await ctx.send('pong')
+  ## ghost ping detetor
+  ghost_ping_mentions = ['@everyone', '@Moderators', '@&901871414355828836', '@RedTachyon19']
+  for word in ghost_ping_mentions:
+    if word in message.content:  
+      ghost_ping_alert = discord.Embed(
+        title = "Ghost-Ping Detected!", 
+        url = "https://discord.gg/VkF3z8WmEN", 
+        description = f"Date: {datetime.date.today()}", 
+        color = 0x9f38f3)
+      
+      ghost_ping_alert.add_field(name = "Message Content", value = message.content, inline = False) 
+      ghost_ping_alert.set_thumbnail(url = "https://media.tenor.com/uSK7LpYPV8MAAAAi/bot-antighostping.gif")
+      ghost_ping_alert.set_footer(icon_url = message.author.avatar.url, text = f"Message Author: {message.author}")
+      await message.channel.send(embed = ghost_ping_alert)
 
-
-#snipe command, press /snipe to reveal the latest deleted post
-#@bot.command(name = 'snipe')
-@slash.slash(name='snipe',
-             description='restores most recently deleted messages',
-             guild_ids=[])
-async def snipe(ctx: SlashContext):
-    channel = ctx.channel
-
-    try:
-        #This piece of code is run if the bot finds anything in the dictionary
-        em = discord.Embed(name=f"Last deleted message in #{channel.name}",
-                           description=snipe_message_content[channel.id])
-        em.set_footer(
-            text=f"This message was sent by {snipe_message_author[channel.id]}"
-        )
-        await ctx.send(embed=em)
-    except:
-        #This piece of code is run if the bot doesn't find anything in the dictionary
-        await ctx.send(
-            f"There are no recently deleted messages in #{channel.name}")
-
-
-## purges chat of previous send messages based on certain amoung
-@slash.slash(name = 'purge',
-  description='purges the last sent messages',
-  guild_ids=[901148844060995604, 821524088492392459, 974480494110584892])
-@commands.has_role('Moderators')
-async def purge(ctx, amount: int):
-    await ctx.channel.purge(limit = amount)
-    await ctx.send(str(amount) + ' messages purged by ' + ctx.author.mention)
-
-@slash.slash(name = 'giverole',
-  description = 'gives a member a specific role',
-  guild_ids = [901148844060995604, 821524088492392459, 974480494110584892])
-@commands.has_role('Moderators')
-async def giverole(ctx, member: discord.Member, role: discord.Role):
-  await ctx.send(ctx.author.mention + ' has given ' + member.mention + ' the role ' + str(role))
-  await member.add_roles(role)
-
-@slash.slash(name='ban',
-  description = 'bans a certain user',
-  guild_ids=[901148844060995604, 821524088492392459, 974480494110584892])
-@commands.has_permissions(ban_members = True)
-async def ban(ctx, member : discord.Member, *, reason = None):
-  await ctx.send(ctx.author.mention + ' has banned ' + member.mention)  
-  await member.ban(reason = reason)
+#snipe command
+@bot.command(description = "restores the most recently deleted message")
+async def snipe(ctx):
+  
+  try:
+    embed = discord.Embed(
+      name = f"Last deleted message in #{ctx.channel.name}", 
+      description = snipe_message_content[ctx.channel.id]
+    )
+    embed.set_footer(
+      text = f"This message was sent by {snipe_message_author[ctx.channel.id]}"
+    )
+    await ctx.respond(embed = embed)
     
-@slash.slash(name='whois',
-  description = 'gives you information about a certain member',
-  guild_ids=[901148844060995604, 821524088492392459, 974480494110584892])
+  except:
+    await ctx.respond(
+      f"There are no recently deleted messages in #{ctx.channel.name}"
+    )
 
-async def whois(ctx, member : discord.Member):
-  em = discord.Embed(
+#ping & latency command
+@bot.command(description = "replies with pong!")
+async def ping(ctx):  
+  await ctx.respond(f"Pong! Latency is {bot.latency}")
+
+#invite command
+@bot.command(description='sends you an invite of this server')
+async def invite(ctx):
+  await ctx.respond("https://discord.gg/VkF3z8WmEN")
+
+#who is command
+@bot.command(description = 'gives you information about a certain member')
+async def whois(ctx, *, member: discord.Member):
+
+  embed = discord.Embed(
     title = "Member Info", 
     url = "https://discord.gg/VkF3z8WmEN", 
-    description = "here is the information regarding a certain member", 
+    description = f"Here is more information about {member.display_name}",
     color = 0x000000)
 
-  em.add_field(name = "Member Name", value = member, inline = False)
-  em.add_field(name = "Member ID", value = member.id, inline = False)
-  em.add_field(name = "Time of Request", value = datetime.date.today(), inline = False)
-  em.set_thumbnail(url = member.avatar_url)
-  em.set_footer(icon_url = ctx.author.avatar_url, text = 'informtion requested by ' + str(ctx.author))
-  await ctx.send(embed = em)
+  embed.add_field(name = "Member Name", value = member, inline = False)
+  embed.add_field(name = "Member ID", value = member.id, inline = False)
+  embed.add_field(name = "Server Join Date", value = member.joined_at, inline = False)
+  embed.add_field(name = "Date of Request", value = datetime.date.today(), inline = False)
+  embed.set_thumbnail(url = member.display_avatar.url)
+  embed.set_footer(icon_url = ctx.author.display_avatar.url, text = f"Request by {ctx.author}")
+  await ctx.respond(embed = embed)
 
-@slash.slash(name='invite',
-  description='sends you an invite of this server',
-  guild_ids=[901148844060995604])
-async def invite(ctx):
-    await ctx.send('https://discord.gg/VkF3z8WmEN')
-  
-keep_alive.keep_alive()
-jalapeño_TOKEN = os.environ['jalapeno_TOKEN']
-bot.run(jalapeño_TOKEN)
+#give role command
+@bot.command(
+  description = 'gives a member a specific role'
+)
+@default_permissions(manage_roles = True)
+
+async def giverole(ctx, member: discord.Member, role: discord.Role):
+  await ctx.respond(ctx.author.mention + ' has given ' + member.mention + ' the role ' + str(role))
+  await member.add_roles(role)
+
+#purge command
+@bot.command(
+  name = 'purge',
+  description='purges the last sent messages',
+)
+@default_permissions(manage_messages = True)
+async def purge(ctx, amount: int):
+    await ctx.channel.purge(limit = amount)
+    await ctx.respond(str(amount) + ' messages purged by ' + ctx.author.mention)
+
+jalapeno_TOKEN = os.environ['jalapeno_TOKEN']
+bot.run(jalapeno_TOKEN)
